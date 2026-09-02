2023d6873eafa89d50cc331ca02c4d46acd60913
Bubka <858858+Bubka@users.noreply.github.com>
2026-09-02 22:20:53 +0200

    Rebuild assets

--- public/build/assets/breakpoints-BVN7CR6g.js
+++
@@ -1,1 +0,0 @@
-import{am as t}from"./app-CM47W83i.js";function a(){return{isLaptop:t({laptop:1024}).laptop}}export{a as u};
---
+++ public/build/assets/bus-ChE1sp8V.js
@@ -0,0 +1,1 @@
+import{an as e}from"./app-dvJP63FL.js";const r=e("bus",{state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
